7c62d1b1d1f5c6d53be92605827c38480192b131
Aguirre Sabino <aguirresabino187@gmail.com>
2018-02-17 16:53:46 -0300

    Removendo configuração antiga do banco de dados

--- db/connect.js
+++
@@ -1,9 +0,0 @@
-const {Client} = require('pg');
-
-const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'projeto1-bd2',
-  password: 'secretpassword',
-  port: 5432,
-}).connect();
