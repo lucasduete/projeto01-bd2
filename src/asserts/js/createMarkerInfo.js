0041fadd12b16e1903ca17ece054797c023fe22c
Lucas Duete <lm.duete@gmail.com>
2018-02-17 20:45:09 -0300

    Modularizando a Criação de Marcadores do Tipo Info.

---
+++ src/asserts/js/createMarkerInfo.js
@@ -0,0 +1,37 @@
+function createMarkerInfo(coordenadas, map) {
+  
+  var marker = new google.maps.Marker({
+      position: coordenadas,
+      map: map,
+      title: '{{local.nome}}'
+  });
+
+
+  var infoBox = '<div id="contentInfoBox"><h1 id="headHeading" class="headHeading"> {{local.nome}} </h1><div id="bodyContent"><p><b> {{local.nome}} </b>, {{local.descricao}} .</p></div><div ng-repeat="(key, value) in local.comentarios"><p><i> {value.nomeUsuario} </i><p id="comentario"> {value.comentario} </p></p></div></div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: infoBox,
+    maxWidth: 200
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+};
+
+      // MODELO
+      // <div id="contentInfoBox">
+      //     <h1 id="headHeading" class="headHeading">{{local.nome}}</h1>
+      //     <div id="bodyContent">
+      //       <p>
+      //         <b>{{local.nome}}</b>, {{local.descricao}}.
+      //       </p>
+      //     </div>
+      //     <div ng-repeat="(key, value) in local.comentarios">
+      //       <p>
+      //         <i>{value.nomeUsuario}</i>
+      //         <p id="comentario">{value.comentario}</p>
+      //       </p>
+      //     </div>
+      //   </div>
