614d8b1c8cda660f9b20ee3a1c9e582d904370f7
Aguirre Sabino <aguirresabino187@gmail.com>
2018-02-17 16:37:29 -0300

    iniciando configuração do express

---
+++ server/config/express.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let logger = require('morgan');
+let bodyParser = require('body-parser');
+
+let api = express();
+
+api.use(logger('dev'));
+
+api.use(bodyParser.json());
+api.use(bodyParser.urlencoded({ extended: false }));
+
+let router = express.Router();
+
+router.use((req, res, next) => {
+	console.log("Algo acontecendo"); //Será impresso em todas as rotas
+	next();
+});
+
+router.get('*', (req, res) => res.status(200).send({
+  message: 'Projeto de Banco de dados II',
+}));
+
+api.use('/api', router); //rota raiz
+
+module.exports = api;
