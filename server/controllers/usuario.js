00c472e06ed3b835dd27fa0040ae63e30769561d
Aguirre Sabino <aguirresabino187@gmail.com>
2018-02-18 13:18:06 -0300

    Adicionando novas rotas

--- server/controllers/usuario.js
+++ server/controllers/usuario.js
@@ -27,7 +27,24 @@ module.exports = {
     },
 
     update(req, res){
-        // codificar
+        return Usuario
+            .findById(req.params.userId)
+            .then(success => {
+                if(!success){
+                    return res.status(404).send({
+                        message: 'Usuario Not Found',
+                    });
+                }
+                return success
+                    .update({
+                        nome: req.body.nome || success.nome,
+                        email: req.body.email || success.email,
+                        senha: req.body.senha || success.senha
+                    })
+                    .then(() => res.status(200).send(success))
+                    .catch((error) => res.status(400).send(error));
+            })
+            .catch(error => res.status(400).send(error));
     },
 
     delete(req, res){
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,4 +3,5 @@ let userController = require('../controllers').usuario;
 module.exports = (api) => {
    api.post('/api/user/cadastro', userController.create);
    api.get('/api/user/:userId', userController.read);
+   api.put('api/user/alterar-dados/:userId', userController.update);
 };
