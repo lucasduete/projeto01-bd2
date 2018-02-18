8115e2e9117fd6b4bc6d21a53e0a47c4bb46d367
Lucas Duete <lm.duete@gmail.com>
2018-02-17 22:25:54 -0300

    Criando cache para o Id da Localidade.

--- src/asserts/js/createMarkerInfo.js
+++ src/asserts/js/createMarkerInfo.js
@@ -6,7 +6,7 @@ function createMarkerInfo(coordenadas, map) {
       title: '{{local.nome}}'
   });
 
-
+  localStorage.idLocal = {{local.id}}
   let infoBox = '<div id="contentInfoBox"><h1 id="headHeading" class="headHeading"> {{local.nome}} </h1><div id="bodyContent"><p><b> {{local.nome}} </b>, {{local.descricao}}.</p></div><div><p><i> {{value.nomeUsuario}} </i><button class="btn blue-grey darken-1"><a href="dadosLocalidade.html"><i class="material-icons left">comment</i>Ver Comentários</a></button></p></div></div>';
 
   let infowindow = new google.maps.InfoWindow({
