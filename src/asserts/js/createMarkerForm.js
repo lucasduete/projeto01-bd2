23eb879c30db9bb8b35b80d9b7278bb47be2fb62
Lucas Duete <lm.duete@gmail.com>
2018-02-19 12:09:23 -0300

    Finalizando a Classe de CreateMarkerForm.js

--- src/asserts/js/createMarkerForm.js
+++ src/asserts/js/createMarkerForm.js
@@ -1,6 +1,6 @@
 function createMarkerForm(coordenadas, map) {
 
-	let formBox = '<div id="content" style="width:330px;"><form><h5 class="flow-text">Cadastro de Marker</h5><input type="text" name="nomeLocal" placeholder="Nome do Local" required><div id="bodyFormContent"><p><input type="text" name="descricaoLocal" placeholder="Digite uma Descricao" required></p><p><button class="waves-effect waves-light red btn" style="margin-right:2%" onclick="location.href=\'cadastarLocal.html\'"><i class="material-icons right">menu</i>Nova Pagina</button><button class="waves-effect waves-light btn" type="submit"><i class="material-icons right">send</i>enviar</button></p></div></form></div>';
+	let formBox = '<div id="content" style="width:330px;"><h5 class="flow-text">Cadastro de Marker</h5><input type="text" id="localNome" name="nomeLocal" placeholder="Nome do Local" required><div id="bodyFormContent"><p><input type="text" id="localDescricao" name="descricaoLocal" placeholder="Digite uma Descricao" required></p><p><button class="waves-effect waves-light red btn" style="margin-right:2%" onclick="location.href=\'cadastarLocal.html\'"><i class="material-icons right">menu</i>Nova Pagina</button><button onclick="localStorage.setItem(\'marcador\', JSON.stringify({latitude : ' + coordenadas.latitude + ',longitude: ' + coordenadas.longitude + ',nome: document.getElementById(\'localNome\').value, descricao: document.getElementById(\'localDescricao\').value,nota: 0,id : 1}));" class="waves-effect waves-light btn"><i class="material-icons right">send</i>enviar</button></p></div></div>';
 
     let marker = new google.maps.Marker({
         position: coordenadas,
@@ -19,18 +19,22 @@ function createMarkerForm(coordenadas, map) {
     map.panTo(coordenadas);
 };
 
+function callCreateMarker() {
+    angular.element('#LocalController').scope().$createMarker();
+}
+
 	// FORMATO DA DIV
-	// <div id="contentFormBox">
+	// <div id="contentFormBox" ng-init="">
  //      <form>
  //        <h1 id="headFormContent" class="headFormContent">
- //          <input type="text" name="nomeLocal" placeholder="Nome do Local" required>
+ //          <input ng-model="Local.nomeLocal" type="text" name="nomeLocal" placeholder="Nome do Local" required>
  //        </h1>
  //        <div id="bodyFormContent">
  //          <p>
- //            <input type="text" name="descricaoLocal" placeholder="Digite uma Descricao" required>
+ //            <input type="text" ng-model="Local.descricaoLocal" name="descricaoLocal" placeholder="Digite uma Descricao" required>
  //          </p>
  //          <p>
- //            <input type="submit">
+ //            <input type="submit" ng-click="Local.cadastrar(Local.nomeLocal, ">
  //          </p>
  //        </div>
  //      </form>
