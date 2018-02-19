a0ebfd0861462df4ad5fa41daa894c51ec0dfa53
Lucas Duete <lm.duete@gmail.com>
2018-02-19 01:33:33 -0300

    Estilizando Cadastro de MarkerForm.

--- src/asserts/js/createMarkerForm.js
+++ src/asserts/js/createMarkerForm.js
@@ -1,6 +1,6 @@
 function createMarkerForm(coordenadas, map) {
 
-	let formBox = '<div id="content" style="330px"><form><h1 id="headFormContent" class="headFormContent"><input type="text" name="nomeLocal" placeholder="Nome do Local" required></h1><div id="bodyFormContent"><p><input type="text" name="descricaoLocal" placeholder="Digite uma Descricao" required></p><p><input type="submit"></p></div></form></div>';
+	let formBox = '<div id="content" style="width:330px;"><form><h5 class="flow-text">Cadastro de Marker</h5><input type="text" name="nomeLocal" placeholder="Nome do Local" required><div id="bodyFormContent"><p><input type="text" name="descricaoLocal" placeholder="Digite uma Descricao" required></p><p><button class="waves-effect waves-light red btn" style="margin-right:2%" onclick="location.href=\'cadastarLocal.html\'"><i class="material-icons right">menu</i>Nova Pagina</button><button class="waves-effect waves-light btn" type="submit"><i class="material-icons right">send</i>enviar</button></p></div></form></div>';
 
     let marker = new google.maps.Marker({
         position: coordenadas,
