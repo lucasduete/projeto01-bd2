f4d29a136856d5e9ef8ce1cb5dd3a3418893a050
Lucas Duete <lm.duete@gmail.com>
2018-02-19 14:48:40 -0300

    controlando a Cor dos Markers de Acordo com a Nota.

--- src/asserts/js/createMarkerInfo.js
+++ src/asserts/js/createMarkerInfo.js
@@ -3,9 +3,23 @@ function createMarkerInfo(marcador, map) {
   let coordenadas = {lat: parseFloat(marcador.latitude), lng: parseFloat(marcador.longitude)};
   let infoBox = '<div id="content" style="width:250px;"><h5 id="headHeading" class="headHeading flow-text"> ' + marcador.nome + ' </h5><div id="bodyContent"><p><b> ' + marcador.nome + ' </b>, ' + marcador.descricao + '.</p></div><div><p><button class="btn cyan darken-1" onclick="location.href=\'dadosLocalidade.html\';"><i class="material-icons left">comment</i>Ver Mais</button></p></div></div>';
 
+  let nota = marcador.nota;
+  var icone_url;
+
+  if (nota <= 2) {
+    icone_url = 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_blue.png';
+  } else if (nota <= 5) {
+    icone_url = 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_green.png';
+  } else if (nota <= 7) {
+    icone_url = 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_orange.png';
+  } else (nota <= 10) {
+    icone_url = 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_red.png;
+  }
+
   let marker = new google.maps.Marker({
       position: coordenadas,
       map: map,
+      icon: icone_url,
       title: marcador.nome
   });
 
