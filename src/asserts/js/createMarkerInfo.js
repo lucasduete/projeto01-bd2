d336958fbffa3c17a0c0c0ad971ff0c94b3a2895
Lucas Duete <lm.duete@gmail.com>
2018-02-20 08:19:11 -0300

    Modificando Estilo dos Marcadores de Info.

--- src/asserts/js/createMarkerInfo.js
+++ src/asserts/js/createMarkerInfo.js
@@ -6,14 +6,16 @@ function createMarkerInfo(marcador, map) {
   let nota = marcador.nota;
   var icone_url;
 
+  //link pra repo dos icones : https://github.com/Concept211/Google-Maps-Markers
+
   if (nota <= 2) {
-    icone_url = 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_blue.png';
+    icone_url = 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_blue.png';
   } else if (nota <= 5) {
-    icone_url = 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_green.png';
+    icone_url = 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_green.png';
   } else if (nota <= 7) {
-    icone_url = 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_orange.png';
+    icone_url = 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_red.png';
   } else if (nota <= 10) {
-    icone_url = 'http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_red.png';
+    icone_url = 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_black.png';
   }
 
   let marker = new google.maps.Marker({
